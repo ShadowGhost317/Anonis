c50b03c3f02da79e7bc4184efa3e18190ece8163
shadowghost <andrescoy19@gmail.com>
2022-12-08 18:37:55 -0500

    Borrando formularios basura

--- paginaw/projectox/www.ianonimo.com/assets/js/bootstrap.js
+++ paginaw/projectox/www.ianonimo.com/assets/js/bootstrap.js
@@ -1451,7 +1451,7 @@ var Dropdown = function ($$$1) {
     DATA_TOGGLE: '[data-toggle="dropdown"]',
     FORM_CHILD: '.dropdown form',
     MENU: '.dropdown-menu',
-    NAVBAR_NAV: '.navbar-nav',
+    BAR_NAV: '.navbar-nav',
     VISIBLE_ITEMS: '.dropdown-menu .dropdown-item:not(.disabled)'
   };
   var AttachmentMap = {
